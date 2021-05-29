ac1ff76a768158de8f3c5f02cac446084850d4e5
AdamRepasky <57711080+AdamRepasky@users.noreply.github.com>
2021-05-29 16:02:03 +0200

    updated token visiting

--- src/main/java/com/mycompany/checks/NegativeIncrementInCycleCheck.java
+++ src/main/java/com/mycompany/checks/NegativeIncrementInCycleCheck.java
@@ -4,28 +4,49 @@ import com.puppycrawl.tools.checkstyle.api.*;
 
 
 public class NegativeIncrementInCycleCheck extends AbstractCheck{
+    private boolean inFor;
+
     @Override
     public int[] getDefaultTokens() {
-        return new int[]{TokenTypes.FOR_ITERATOR};
+        return new int[]{TokenTypes.FOR_ITERATOR, TokenTypes.POST_DEC, TokenTypes.DEC, TokenTypes.MINUS_ASSIGN};
     }
 
     @Override
     public int[] getAcceptableTokens() {
-        return new int[]{TokenTypes.FOR_ITERATOR};
+        return new int[]{TokenTypes.FOR_ITERATOR, TokenTypes.POST_DEC, TokenTypes.DEC, TokenTypes.MINUS_ASSIGN};
     }
 
     @Override
     public int[] getRequiredTokens() {
-        return new int[]{TokenTypes.FOR_ITERATOR};
+        return new int[]{TokenTypes.FOR_ITERATOR, TokenTypes.POST_DEC, TokenTypes.DEC, TokenTypes.MINUS_ASSIGN};
     }
 
     @Override
     public void visitToken(DetailAST ast)
     {
-        if(ast.branchContains(TokenTypes.POST_DEC) || ast.branchContains(TokenTypes.DEC) || ast.branchContains(TokenTypes.MINUS_ASSIGN)){
-            String message = "Negative increment in cycle declaration found.";
-            log(ast.getLineNo(), message);
+        if (ast.getType() == TokenTypes.FOR_ITERATOR) {
+            this.inFor = true;
+        }
+
+        if (inFor) {
+            if (ast.getType() == TokenTypes.POST_DEC) {
+                String message = "Negative increment in cycle declaration found A.";
+                log(ast.getLineNo(), message);
+            }
+            if (ast.getType() == TokenTypes.DEC) {
+                String message = "Negative increment in cycle declaration found B.";
+                log(ast.getLineNo(), message);
+            }
+            if (ast.getType() == TokenTypes.MINUS_ASSIGN) {
+                String message = "Negative increment in cycle declaration found C.";
+                log(ast.getLineNo(), message);
+            }
         }
     }
-
+    @Override
+    public void leaveToken(DetailAST ast) {
+        if (ast.getType() == TokenTypes.FOR_ITERATOR) {
+            this.inFor = false;
+        }
+    }
 }
